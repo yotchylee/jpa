278da943f20cc3451155a7f114388f8e699c8d67
yotchylee <zhang_xg1990@163.com>
2020-04-12 20:31:40 +0800

    jpa project

---
+++ src/main/java/com/example/accessingdatajpa/RepositoryConfigurations.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatajpa;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+//@Configuration
+//@EntityScan("com.example.accessingdatajpa")
+//@ConditionalOnProperty("spring.datasource.driverClassName")
+//@EnableJpaRepositories(basePackages = "com.example.accessingdatajpa")
+public class RepositoryConfigurations {
+}
---
+++ src/main/java/com/example/accessingdatajpa/User.java
@@ -0,0 +1,47 @@
+package com.example.accessingdatajpa;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@ApiModel(value = "user对象")
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+  @ApiModelProperty(value = "主键",example="10")
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+
+  @ApiModelProperty(value = "姓名",example="张三")
+  private String name;
+
+  @ApiModelProperty(value = "邮箱",example="google@gmail.com")
+  private String email;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
